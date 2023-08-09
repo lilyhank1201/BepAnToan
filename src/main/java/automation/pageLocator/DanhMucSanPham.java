3267f611970170ffd2dd110d29e7e851fcc0a1db
lilyhank1201 <135857381+lilyhank1201@users.noreply.github.com>
2023-08-09 16:23:25 +0700

    updatetrangnth

--- src/main/java/automation/pageLocator/DanhMucSanPham.java
+++ src/main/java/automation/pageLocator/DanhMucSanPham.java
@@ -1,65 +1,70 @@
 package automation.pageLocator;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class DanhMucSanPham {
-	WebElement driver;
-	@FindBy(xpath = "//span[contains(text(),'Danh mục sản phẩm')]")
-	private WebElement btnDanhMucSanPham;
-	@FindBy(xpath = "//div[contains(text(),'Bếp Từ')]")
-	private WebElement btnBepTu;
-	@FindBy(xpath = "//img[@src='https://static.bepantoan.vn/userfiles/images/456.jpg?w=100']")
-	private WebElement HangSanXuatKaiNer;
-	@FindBy(xpath = "//img[@src='https://static.bepantoan.vn/userfiles/images/Ảnh chụp Màn hình 2021-09-05 lúc 00_21_49.png?w=100']")
-	private WebElement HangSanXuatBinova;
-	@FindBy(xpath = "//img[@src='chrome-extension://mcgbeeipkmelnpldkobichboakdfaeon/images/logo-vertical.svg']")
-	private WebElement HangSanXuat;
-	@FindBy(xpath = "//span[contains(text(),'< 3.000.000')]")
-	private WebElement MucgiaNhoHon3tr;
-	@FindBy(xpath = "//span[contains(text(),'3.000.000 > 5.000.000')]")
-	private WebElement Mucgiatu3den5tr;
-	@FindBy(xpath = "//span[contains(text(),'5.000.000 > 10.000.000')]")
-	private WebElement Mucgiatu5den10tr;
-	@FindBy(xpath = "//span[contains(text(),'10.000.000 > 15.000.000')]")
-	private WebElement Mucgiatu10den15tr;
-	@FindBy(xpath = "(//span[contains(text(),' > 15.000.000')])[1]")
-	private WebElement MucgiaLonHon15tr;
-	@FindBy(xpath = "//span[contains(text(),'Chính Hãng')]")
-	private WebElement btnXuatXuChinhHang; 
-	@FindBy(xpath = "//span[contains(text(),'Italy')]")
-	private WebElement btnXuatXuItaly;
-	@FindBy(xpath = "//span[contains(text(),'1 bếp')]")
-	private WebElement btnMotBep; 
-	@FindBy(xpath = "//span[contains(text(),'2 bếp')]")
-	private WebElement btnHaiBep;
-	@FindBy(xpath = "(//span[contains(text(),'Bếp từ')])[1] ")
-	private WebElement btnBepTuPhanLoai; 
+	WebDriver driver;
+	
+	
+	private By btnDanhMucSanPham = By.xpath("//span[contains(text(),'Danh mục sản phẩm')]");
+ 
+	
+	private By btnBepTu = By.xpath("//div[contains(text(),'Bếp Từ')]");
 
-	public DanhMucSanPham(WebDriver driver) {
-		this.driver = (WebElement) driver;
-		PageFactory.initElements(driver, this);
+	private By HangSanXuatKaiNer = By.xpath("//img[@src='https://static.bepantoan.vn/userfiles/images/456.jpg?w=100']");
+	
+	private By btnMuaTraGop = By.xpath("(//span[contains(text(),'Mua trả góp')])[1]");
+	private By HangSanXuatBinova = By.xpath("//img[@src='https://static.bepantoan.vn/userfiles/images/Ảnh chụp Màn hình 2021-09-05 lúc 00_21_49.png?w=100']"); 
+	private By HangSanXuat = By.xpath("//img[@src='chrome-extension://mcgbeeipkmelnpldkobichboakdfaeon/images/logo-vertical.svg']");  
+
+	private By MucgiaNhoHon3tr = By.xpath("//span[contains(text(),'< 3.000.000')]");
+	private By Mucgiatu3den5tr = By.xpath( "//span[contains(text(),'3.000.000 > 5.000.000')]");
+	private By Mucgiatu5den10tr = By.xpath( "//span[contains(text(),'5.000.000 > 10.000.000')]");
+	private By Mucgiatu10den15tr = By.xpath( "//span[contains(text(),'10.000.000 > 15.000.000')]");
+	private By MucgiaLonHon15tr = By.xpath( "(//span[contains(text(),' > 15.000.000')])[1]");
+	private By btnXuatXuChinhHang = By.xpath( "//span[contains(text(),'Chính Hãng')]");
+	private By btnXuatXuItaly = By.xpath("//span[contains(text(),'Italy')]");
+	private By btnMotBep = By.xpath("//span[contains(text(),'1 bếp')]");
+	private By btnHaiBep = By.xpath("//span[contains(text(),'2 bếp')]");
+	private By btnBepTuPhanLoai = By.xpath("(//span[contains(text(),'Bếp từ')])[1] ");
+
+	public  DanhMucSanPham (WebDriver _driver) {
+		this.driver = _driver;
+	}  
+	
+	
+	public void DanhMucSP() {
+		WebElement clickDanhMucSP = driver.findElement(btnDanhMucSanPham);
+		if (clickDanhMucSP.isDisplayed()) {
+			clickDanhMucSP.click();
+		}
 	}
-
-	public void GotoDetail( ) throws InterruptedException {
-		btnDanhMucSanPham.click();
-		btnBepTu.click();
-		Thread.sleep(2000);
-		HangSanXuatBinova.click();
-		Thread.sleep(2000);
-
-		Mucgiatu10den15tr.click();
-		Thread.sleep(2000);
-
-		btnXuatXuItaly.click();
-		Thread.sleep(2000);
-
-		btnHaiBep.click();		
-		Thread.sleep(2000);
-
-		btnBepTuPhanLoai.click();  
-		
+	public void HangSanXuatBinova() {
+		WebElement chooseHangSanXuatBinova = driver.findElement(HangSanXuatBinova);
+		if (chooseHangSanXuatBinova.isDisplayed()) {
+			chooseHangSanXuatBinova.click();
+		}
 	}
+	
+	public void Mucgiatu10den15tr() {
+		WebElement chooseMucgiatu10den15tr = driver.findElement(Mucgiatu10den15tr);
+		if (chooseMucgiatu10den15tr.isDisplayed()) {
+			chooseMucgiatu10den15tr.click();
+		}
+	}
+	public void btnXuatXuItaly() {
+		WebElement choosebtnXuatXuItaly = driver.findElement(btnXuatXuItaly);
+		if (choosebtnXuatXuItaly.isDisplayed()) {
+			choosebtnXuatXuItaly.click();
+		}
+	}
+	public void btnHaiBep() {
+		WebElement clickbtnHaiBep = driver.findElement(btnHaiBep);
+		if (clickbtnHaiBep.isDisplayed()) {
+			clickbtnHaiBep.click();
+		}
+	}
+	 
 }
